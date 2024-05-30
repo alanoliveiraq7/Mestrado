6681c967a6a5992f95e8c93931090bc1d76ad40a
alanoliveiraq7 <alanoliveira2282@gmail.com>
2024-05-30 08:12:16 -0300

    Update Euler.f90

--- Euler.f90
+++ Euler.f90
@@ -6,6 +6,12 @@ program Euler
     character(len=100) :: output_filename
     integer :: iunit
 
+    interface
+        real(8) function taxa_reacao(conc_A, conc_B, k)
+            real(8), intent(in) :: conc_A, conc_B, k
+        end function taxa_reacao
+    end interface
+
     ! Valores iniciais
     t = 0.0
     A = 1.0
@@ -21,12 +27,6 @@ program Euler
     allocate(B_values(n_steps))
     allocate(C_values(n_steps))
 
-    ! Função para a taxa de reação
-    real(8) function taxa_reacao(conc_A, conc_B)
-        real(8), intent(in) :: conc_A, conc_B
-        taxa_reacao = k * conc_A * conc_B
-    end function taxa_reacao
-
     ! Aplicação do método de Euler e armazenamento dos valores
     do n = 1, n_steps
         A_values(n) = A
@@ -34,9 +34,9 @@ program Euler
         C_values(n) = C
 
         ! Atualização das concentrações de A e B
-        A = A - dt * taxa_reacao(A, B)
-        B = B - dt * taxa_reacao(A, B)
-        C = C + dt * taxa_reacao(A, B)
+        A = A - dt * taxa_reacao(A, B, k)
+        B = B - dt * taxa_reacao(A, B, k)
+        C = C + dt * taxa_reacao(A, B, k)
         t = t + dt
     end do
 
@@ -54,3 +54,9 @@ program Euler
 
     stop
 end program Euler
+
+! Função para a taxa de reação
+real(8) function taxa_reacao(conc_A, conc_B, k)
+    real(8), intent(in) :: conc_A, conc_B, k
+    taxa_reacao = k * conc_A * conc_B
+end function taxa_reacao
