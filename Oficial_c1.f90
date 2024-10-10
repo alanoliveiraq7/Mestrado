df50f782d8d22ff73da039628184804ae62b389b
alanoliveiraq7 <alanoliveira2282@gmail.com>
2024-10-10 17:56:22 -0300

    Atualizar o Oficial_c1.f90

---
+++ Oficial_c1.f90
@@ -0,0 +1,200 @@
+program main
+implicit none
+real*8, dimension(6) :: v,w,u,k1,k2,k3,k4
+real*8 tf,h,t,a,cAni
+integer cont,cont2,i,j,k,cont3,crit,nrint,nrpoints,crit2,ni
+integer conts,conts2
+integer, parameter :: lk = selected_int_kind(18)
+real*8 Cd,Ntot,Faraday,eappm,rm,cF,cFA,cWater,frt,ratek2,kcpl
+character(len=1) :: nfac
+real*8 :: EvSL                ! um teste
+Cd = 24d-6
+ratek2 = 1.1d2
+Ntot = 2.18d-9
+Faraday = 9.648533d4
+cF = 3.55d-7
+cFA = 1.99d-4
+cWater = 55d-3
+
+frt = Faraday / (8.314d0 * 298.15d0)
+
+cAni = 1.0d-1
+rm = 3.0d3
+
+h = 1d-5
+tf = 1d2
+t = 0d0
+nrint = int(tf / h)
+nrpoints = 1000
+crit2 = 2
+crit = 10000
+write(6,*) 'critério', crit
+
+do ni = 1, 1
+    write(6,*) 'j factor', ni
+
+    kcpl = -9d2
+    !eapps0 = 9.1d-1
+    eappm = 8.9d-1
+
+    t = 0d0
+    write(nfac,'(i1)') ni
+
+    open(unit = 401, file = 'em9'//nfac//'FB.dat')
+    open(unit = 402, file = 'em9'//nfac//'FL.dat')
+    open(unit = 403, file = 'em9'//nfac//'CO.dat')
+    open(unit = 404, file = 'em9'//nfac//'OH.dat')
+    open(unit = 405, file = 'em9'//nfac//'FI.dat')
+    open(unit = 406, file = 'em9'//nfac//'ANI.dat')
+    open(unit = 407, file = 'EvSL.dat')  ! um teste
+
+    cont = 0
+    cont2 = 0
+
+    v = (/0d0, 0d0, 0.58d0, 0d0, 0.05d0, cAni/)
+
+    cont3 = 0
+    do k = 1, 4
+        cont3 = cont3 + 1
+
+        do i = 1, 1000000
+            call uf(v)
+            do j = 1, size(u)
+                k1(j) = u(j)
+            enddo
+
+            do j = 1, size(v)
+                w(j) = v(j) + h * k1(j) / 2d0
+            enddo
+            call uf(w)
+            do j = 1, size(u)
+                k2(j) = u(j)
+            enddo
+
+            do j = 1, size(v)
+                w(j) = v(j) + h * k2(j) / 2d0
+            enddo
+            call uf(w)
+            do j = 1, size(u)
+                k3(j) = u(j)
+            enddo
+
+            do j = 1, size(v)
+                w(j) = v(j) + h * k3(j)
+            enddo
+            call uf(w)
+            do j = 1, size(u)
+                k4(j) = u(j)
+            enddo
+
+            do j = 1, size(v)
+                v(j) = v(j) + h * (k1(j) + 2d0 * k2(j) + 2d0 * k3(j) + k4(j)) / 6d0
+            enddo
+
+            if (v(1).ne.v(1)) then
+                write(11,*) cont3,i
+                exit
+            end if
+
+            cont = cont + 1
+            t = t + h
+
+            if (cont.eq.crit) then
+                write(401,*) t,v(1)
+                write(402,*) t,v(2)
+                write(403,*) t,v(3)
+                write(404,*) t,v(4)
+                write(405,*) t,v(5)
+                write(406,*) t,v(6)
+                EvSL = vac(v) ! um teste
+                write(407,*) t, EvSL  ! um teste
+                cont2 = cont2 + 1
+                cont = 0
+                write(6,*) "n =",ni,"   saving  =",cont2
+            end if
+
+            !eapps = eapps0 + kcpl * ( ((eappm-v(5))/rm) - ((eapps-v(5))/rm) )
+        enddo
+    enddo
+enddo
+
+contains
+
+real function kox(beta,fi0,fi)
+    real*8, intent(in) :: beta,fi0,fi
+    kox = dexp(beta * frt * (fi - fi0))
+end function
+
+real function kred(beta,fi0,fi)
+    real*8, intent(in) :: beta,fi0,fi
+    kred = dexp(-(1d0 - beta) * frt * (fi - fi0))
+end function
+
+real function v1(x)
+    real*8, intent(in) :: x(:)
+    v1 = cFA * vac(x) * vac(x) * kox(5d-1, -4d-2, x(5))
+end function
+
+real function v1r(x)
+    real*8, intent(in) :: x(:)
+    v1r = x(1) * kred(5d-1, 2d-2, x(5))
+end function
+
+real function v2(x)
+    real*8, intent(in) :: x(:)
+    v2 = ratek2 * x(1) * vac(x) * vac(x)
+end function
+
+real function v3(x)
+    real*8, intent(in) :: x(:)
+    v3 = x(2) * vac(x) * kox(5d-1, 3.8d-1, x(5))
+end function
+
+real function v4(x)
+    real*8, intent(in) :: x(:)
+    v4 = x(2) * vac(x) * vac(x) * kred(5d-1, 6d-1, x(5))
+end function
+
+real function v5(x)
+    real*8, intent(in) :: x(:)
+    v5 = cWater * vac(x) * kox(6.5d-1, 4d-1, x(5))
+end function
+
+real function v5r(x)
+    real*8, intent(in) :: x(:)
+    v5r = x(4) * kred(6.5d-1, 7.1d-1, x(5))
+end function
+
+real function v6(x)
+    real*8, intent(in) :: x(:)
+    v6 = x(3) * x(4) * kox(5d-1, 7.8d-1, x(5))
+end function
+
+real function v7(x)
+    real*8, intent(in) :: x(:)
+    v7 = cF * vac(x) * kox(5d-1, -4d-1, x(5))
+end function
+
+real function v8(x)
+    real*8, intent(in) :: x(:)
+    v8 = cAni * vac(x) * kox(4.5d-1, 3d-1, x(5))
+end function
+
+real function v8r(x)
+    real*8, intent(in) :: x(:)
+    v8r = x(5) * kred(4.5d-1, 6d-1, x(5))
+end function
+
+
+real function f1(x)
+    real*8, intent(in) :: x(:)
+    f1 = v1(x) - v1r(x) - v2(x)
+end function
+
+real function f2(x)
+    real*8, intent(in) :: x(:)
+    f2 = v2(x) - v3(x) - v4(x)
+end function
+
+real function f3(x)
+    real*
